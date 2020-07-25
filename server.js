b55c1ba6862f7404298ba36fe6438ffa88c5d4c2
rohitludbe <rohitludbe290@gmail.com>
2020-07-25 22:30:45 +0530

    backend login & register created

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+
+const app = express();
+
+// bodyparser middleware
+app.use(express.json());
+
+// connect to db
+const db = config.get("mongoURI");
+
+// connect to db
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("mongoDB connected"))
+  .catch((err) => console.log(err));
+
+// use routes
+app.use("/api/user", require("./routes/api/user"));
+app.use("/api/auth", require("./routes/api/auth"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server start on port ${port}`));
